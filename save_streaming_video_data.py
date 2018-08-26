1ca9b5a170cc78338f8622dd57519655ac6cffad
Siyuan Zhang <siyuanzhang@Siyuans-MacBook-Pro.local>
2018-08-26 16:26:35 -0700

    add release to video write

--- save_streaming_video_data.py
+++ save_streaming_video_data.py
@@ -3,7 +3,10 @@ import cv2
 import urllib2
 import numpy as np
 from datetime import datetime
+import time
+import traceback
 import os
+import sys
 
 
 # Example: python save_streaming_video_data.py --host raspberrypi.local
@@ -22,31 +25,40 @@ out = cv2.VideoWriter('output.mov',fourcc, 20.0, (320,240))
 file_path = str(os.path.dirname(os.path.realpath(__file__)))+"/video_timestamps.txt"
 stream = urllib2.urlopen('http://{host}/webcam.mjpeg'.format(host=host))
 
-bytes = bytes()
-while True:
-    bytes += stream.read(1024)
-    a = bytes.find(b'\xff\xd8')
-    b = bytes.find(b'\xff\xd9')
-    if a != -1 and b != -1:
-        jpg = bytes[a:b+2]
-        bytes = bytes[b+2:]
-        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-        #cv2.imshow('Car Camera', frame)
-        now = datetime.now()
-        print(now)
-        if frame is not None:
-          # cv2.imshow("car camera", frame)
+try:
+    bytes = bytes()
+    while True:
+        bytes += stream.read(1024)
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            #cv2.imshow('Car Camera', frame)
+            now = datetime.now()
+            print(now)
+            if frame is not None:
+              # cv2.imshow("car camera", frame)
 
-          # Use the code below if I need find the dimensions of the video
-          '''
-          height, width, channels = frame.shape
-          print(height)
-          print(width)
-          '''
-          out.write(frame)
-          timestamp = datetime.now()
-          with open(file_path,"a") as writer:
-            writer.write(str(timestamp)+"\n")
-
-        if cv2.waitKey(1) == 27:
-            exit(0)
+              # Use the code below if I need find the dimensions of the video
+              '''
+              height, width, channels = frame.shape
+              print(height)
+              print(width)
+              '''
+              out.write(frame)
+              timestamp = datetime.now()
+              with open(file_path,"a") as writer:
+                writer.write(str(timestamp)+"\n")
+except KeyboardInterrupt:
+    print('Interrupted')
+    try:
+        print('Trying to release VideoWriter.')
+        time.sleep(5)
+        out.release()
+        sys.exit(0)
+    except Exception:
+        print('Failed to release VideoWriter.')
+        print(traceback.format_exc())
+        sys.exit(1)
